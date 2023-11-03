49c36320c11598397913fac0c6c9e9fed794e7a5
Hole333 <123298895+Hole333@users.noreply.github.com>
2023-11-03 13:28:20 +0800

    Add files via upload

---
+++ DEBUG_SmartCar_main/project/code/MyDrivers/Inc/Bluetooth.h
@@ -0,0 +1,13 @@
+#ifndef _BLUETOOTH_H
+#define _BLUETOOTH_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "zf_common_headfile.h"
+/* ---------------------------------------------------------------------------*/
+
+
+/*º¯ÊýÉùÃ÷--------------------------------------------------------------------*/
+void Bluetooth_Init(void);
+void Bluetooth_Send_Float(const float f_num);
+
+#endif
